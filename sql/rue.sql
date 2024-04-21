3348515067dcee68813a56a7b9a132b450024ce5
Sami <sami.bahij@gmail.com>
2024-04-21 15:08:36 +0200

    modification du header et de l'insert sql rue

--- sql/rue.sql
+++ sql/rue.sql
@@ -1,34 +1,34 @@
 -- Insertions pour Poissy
-INSERT INTO street (city_id, name, created_at) VALUES (5, 'Rue du Château', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (5, 'Avenue de l\'Europe', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (5, 'Allée des Marronniers', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (1, 'Rue du Château', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (1, 'Avenue de l\'Europe', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (1, 'Allée des Marronniers', '2024-04-21 10:15:31');
 
 -- Insertions pour Paris
-INSERT INTO street (city_id, name, created_at) VALUES (6, 'Rue de Rivoli', '2024-04-21 10:16:47');
-INSERT INTO street (city_id, name, created_at) VALUES (6, 'Avenue des Champs-Élysées', '2024-04-21 10:16:47');
-INSERT INTO street (city_id, name, created_at) VALUES (6, 'Boulevard Saint-Germain', '2024-04-21 10:16:47');
+INSERT INTO street (city_id, name, created_at) VALUES (2, 'Rue de Rivoli', '2024-04-21 10:16:47');
+INSERT INTO street (city_id, name, created_at) VALUES (2, 'Avenue des Champs-Élysées', '2024-04-21 10:16:47');
+INSERT INTO street (city_id, name, created_at) VALUES (2, 'Boulevard Saint-Germain', '2024-04-21 10:16:47');
 
 -- Insertions pour Achères-Ville
-INSERT INTO street (city_id, name, created_at) VALUES (8, 'Rue de Montesson', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (8, 'Rue du 8 Mai', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (8, 'Avenue Wolfgang Amadeus Mozart', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (3, 'Rue de Montesson', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (3, 'Rue du 8 Mai', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (3, 'Avenue Wolfgang Amadeus Mozart', '2024-04-21 10:15:31');
 
 -- Insertions pour Saint-Germain-En-Laye
-INSERT INTO street (city_id, name, created_at) VALUES (9, 'Rue des Louviers', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (9, 'Rue du Marechal Foch', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (9, 'Avenue des Loges', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (4, 'Rue des Louviers', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (4, 'Rue du Marechal Foch', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (4, 'Avenue des Loges', '2024-04-21 10:15:31');
 
 -- Insertions pour Sartrouville
-INSERT INTO street (city_id, name, created_at) VALUES (10, 'Rue Berthelot', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (10, 'Avenue Jean Jaurès', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (10, 'Boulevard de la République', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (5, 'Rue Berthelot', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (5, 'Avenue Jean Jaurès', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (5, 'Boulevard de la République', '2024-04-21 10:15:31');
 
 -- Insertions pour Orgeval
-INSERT INTO street (city_id, name, created_at) VALUES (11, 'Rue de la Chapelle', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (11, 'Route de Quarante Sous', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (11, 'Chemin des Granges', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (6, 'Rue de la Chapelle', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (6, 'Route de Quarante Sous', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (6, 'Chemin des Granges', '2024-04-21 10:15:31');
 
 -- Insertions pour Le Pecq
-INSERT INTO street (city_id, name, created_at) VALUES (12, 'Rue de Paris', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (12, 'Avenue Charles de Gaulle', '2024-04-21 10:15:31');
-INSERT INTO street (city_id, name, created_at) VALUES (12, 'Quai Voltaire', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (6, 'Rue de Paris', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (6, 'Avenue Charles de Gaulle', '2024-04-21 10:15:31');
+INSERT INTO street (city_id, name, created_at) VALUES (6, 'Quai Voltaire', '2024-04-21 10:15:31');
