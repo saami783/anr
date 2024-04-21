fbf89fbe7231c900458be37e44fdb76915a57c50
Sami <sami.bahij@gmail.com>
2024-04-21 14:57:09 +0200

    Suppression de symfony/mercure-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,6 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
